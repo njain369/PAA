2fa454790f20f8ca9667612faa6f575c4ef929f7
Niraj Jain <niraj.sj@somaiya.edu>
2021-04-08 12:14:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def intro():
+    return render_template('./home.html')    
+
+@app.route('/<name>')
+def index(name):
+    return '<h1>Hello {}!</h1>'.format(name)
+
+@app.route('/home')
+def home():
+    return render_template('home.html') 
+
+
+if(__name__=='__main__'):
+    app.run()
